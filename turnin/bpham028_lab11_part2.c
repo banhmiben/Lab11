773da9df6e0a321bdb015556c2caec00d64b0d01
Ben <benpham_12@yahoo.com>
2021-02-21 20:37:42 -0800

    finished parts 1-3

--- turnin/bpham028_lab11_part2.c
+++ turnin/bpham028_lab11_part2.c
@@ -1,252 +1,358 @@
 /*	Author: lab
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #11  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <timer.h>
 #include <scheduler.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-unsigned char go = 0x00;
-unsigned char tmpA;
-
 void transmit_data(unsigned char data) {
 	int i;
 	for (i = 0; i < 8; ++i) {
-		//Sets SRCLR to 1 allowing data to be set 
+		//Sets SRCLR to 1 allowing data to be set
 		//Also clears SRCLK in preparation of sending data
 		PORTC = 0x08;
 		//set SER = next bit of data to be sent.
 		PORTC |= ((data >> i) & 0x01);
-		//set SRCLK = 1. Rising edge shifts next bit of data into the shift register 
+		//set SRCLK = 1. Rising edge shifts next bit of data into the shift register
 		PORTC |= 0x02;
 	}
 	//set RCLK = 1. Rising edge copies data from "Shift" register to "Storage" register
 	PORTC |= 0x04;
 	//clears all lines in preparation of a new transmission
 	PORTC = 0x00;
-} 
+}
 
-enum CycleStates {Start, CInit, Wait, Power, Release, WaitUp, Up, WaitDown, Down};
+#define lightoff 0x00
+#define lightstate1 0x01
+#define lightstate2 0x02
+#define lightstate3 0x03
+unsigned char go = 0x00;
+unsigned char tmpA;
+
+
+enum CycleStates {init0, off, offwait, release, default_state, first_state, third_state};
 int CycleSMTick(int state) {
-	
-	tmpA = ~PINA & 0x07;
-	switch(state) {
-		case(Start): state = CInit; break;
-		case(CInit): state = Wait; break;
-		case(Wait):
-			if (tmpA == 0x03) {
-				state = Power;
-			} else {
-				state = Wait;
-			} break;
-		case(Power):
-			state = Release;
+
+	switch(state){
+		case(init0):
+			state = off;
 			break;
-		case(Release):
+		case(off):
 			if (tmpA == 0x03) {
-				state = CInit;
-			} else if (tmpA == 0x01) {
-				state = WaitUp;
-			} else if (tmpA == 0x02) {
-				state = WaitDown;
+				state = release;
 			} break;
-		case(WaitUp):
-			state = Up; break;
-		case(Up): 
+		case(release):
+			if (tmpA == 0x00) {
+				state = default_state;
+			} break;
+		case(default_state):
 			if (tmpA == 0x01) {
-				state = Up;
-			} else {
-				state = Release;
+				state = third_state;
+			} else if (tmpA == 0x02) {
+				state = first_state;
+			} else if (tmpA == 0x03) {
+				state = offwait;
 			} break;
-		case(WaitDown):
-			state = Down; break;
-		case(Down):
-			if (tmpA == 0x02) {
-				state = Down;
-			} else {
-				state = Release;
+		case(third_state):
+			if (tmpA == 0x01) {
+				state = first_state;
+			} else if (tmpA == 0x02) {
+				state = default_state;
+			} else if (tmpA == 0x03) {
+				state = offwait;
 			} break;
-		default: state = Start; break;
+		case(first_state):
+			if (tmpA == 0x01) {
+				state = default_state;
+			} else if (tmpA == 0x02) {
+				state = third_state;
+			} else if (tmpA == 0x03) {
+				state = offwait;
+			} break;
+		case(offwait):
+			if (tmpA == 0x00) {
+				state = off;
+			} break;
+		default:
+			state = init0;
+			break;
 	}
 	switch(state) {
-		case(Start): break;
-		case(CInit): 
-			go = 0x00;
+		case(init0):
+			go = lightoff;
 			transmit_data(0x00);
 			break;
-		case(Wait): break;
-		case(Power):
-			go = 0x01;
+		case(off):
 			break;
-		case(Release): break;
-		case(WaitUp):
-			if (go == 0x01) {
-				go = 0x02;
-			} else if (go == 0x02) {
-				go = 0x03;
-			} else if (go == 0x03) {
-				go = 0x01;
-			} break;
-		case(Up): break;
-		case(WaitDown):
-			if (go == 0x01) {
-				go = 0x03;
-			} else if (go == 0x02) {
-				go = 0x01;
-			} else if (go == 0x03) {
-				go = 0x02;
-			} break;
-		case(Down): break;
-		default: break;
+		case(release):
+			break;
+		case(default_state):
+			go = lightstate2;
+			break;
+		case(third_state):
+			go = lightstate3;
+			break;
+		case(first_state):
+			go = lightstate1;
+			break;
+		case(offwait):
+			go = lightoff;
+			transmit_data(0x00);
+			break;
+		default:
+			break;
 	}
 	return state;
 }
 
-enum FestiveOne {Start1, Init, One, Two, Three, Four}; //go == 0x01
-int FestiveOneSMTick(int state) {
-	
+
+enum FestiveFirst {Init, One, Two, Three, Four}; //go == 0x01
+int FestiveFirstSMTick(int state) {
+
 	switch(state) {
-		case(Start1): state = Init; break;
-		case(Init): 
-			if (go == 0x01) {
+		case(Init):
+			if (go == lightstate1) {
 				state = One;
 			} break;
-		case(One): state = Two; break;
-		case(Two): state = Three; break;
-		case(Three): state = Four; break;
-		case(Four): state = One; break;
-		default: state = Init; break;
-	}
-	switch(state) {
-		case(Init):
-			transmit_data(0x00);
-			break;
-		case(One): transmit_data(0x81); break;
-		case(Two): transmit_data(0x42); break;
-		case(Three): transmit_data(0x24); break;
-		case(Four): transmit_data(0x18); break;
-		default: break;
-	}
-	return state;
-}
-
-enum FestiveTwo {Start2, Init1, One1, Two1, Three1, Four1}; //go == 0x02
-int FestiveTwoSMTick(int state) {
-	switch(state) {
-		case(Start2): state = Init1; break;
-		case(Init1): 
-			if (go == 0x02) {
-				state = One1;
+		case(One):
+			if (go == lightstate1) {
+				state = Two;
+			} else {
+				state = Init;
 			} break;
-		case(One1): state = Two1; break;
-		case(Two1): state = Three1; break;
-		case(Three1): state = Four1; break;
-		case(Four1): state = One1; break;
-		default: state = Init1; break;
+		case(Two):
+			if (go == lightstate1) {
+				state = Three;
+			} else {
+				state = Init;
+			} break;
+		case(Three):
+			if (go == lightstate1) {
+				state = Four;
+			} else {
+				state = Init;
+			} break;
+		case(Four):
+			if (go == lightstate1) {
+				state = One;
+			} else {
+				state = Init;
+			} break;
+		default:
+			state = Init;
+			break;
 	}
 	switch(state) {
 		case(Init):
-			transmit_data(0x00);
 			break;
-		case(One1): transmit_data(0xA0); break;
-		case(Two1): transmit_data(0x0A); break;
-		case(Three1): transmit_data(0x50); break;
-		case(Four1): transmit_data(0x05); break;
-		default: break;
+		case(One):
+			transmit_data(0x81); break;
+		case(Two):
+			transmit_data(0x42); break;
+		case(Three):
+			transmit_data(0x24); break;
+		case(Four):
+			transmit_data(0x18); break;
+		default:
+			break;
 	}
 	return state;
 }
 
-enum FestiveThree {Start3, Init2, One2, Two2, Three2, Four2, Five, Six, Seven, Eight};
-int FestiveThreeSMTick(int state) {
+enum FestiveDefault {Init2, One2, Two2, Three2, Four2, Five, Six, Seven, Eight};
+int FestiveDefaultSMTick(int state) {
+
 	switch(state) {
-		case(Start3): state = Init2;
-		case(Init2): 
-			if (go == 0x02) {
+		case(Init2):
+			if (go == lightstate2) {
 				state = One2;
 			} break;
-		case(One2): state = Two2; break;
-		case(Two2): state = Three2; break;
-		case(Three2): state = Four2; break;
-		case(Four2): state = Five; break;
-		case(Five): state = Six; break;
-		case(Six): state = Seven; break;
-		case(Seven): state = Eight; break;
-		case(Eight): state = One2; break;
-		default: state = Init; break;
+		case(One2):
+			if (go == lightstate2) {
+				state = Two2;
+			} else {
+				state = Init2;
+			} break;
+		case(Two2):
+			if (go == lightstate2) {
+				state = Three2;
+			} else {
+				state = Init2;
+			} break;
+		case(Three2):
+			if (go == lightstate2) {
+				state = Four2;
+			} else {
+				state = Init2;
+			} break;
+		case(Four2):
+			if (go == lightstate2) {
+				state = Five;
+			} else {
+				state = Init2;
+			} break;
+		case(Five):
+			if (go == lightstate2) {
+				state = Six;
+			} else {
+				state = Init2;
+			} break;
+		case(Six):
+			if (go == lightstate2) {
+				state = Seven;
+			} else {
+				state = Init2;
+			} break;
+		case(Seven):
+			if (go == lightstate2) {
+				state = Eight;
+			} else {
+				state = Init2;
+			} break;
+		case(Eight):
+			if (go == lightstate2) {
+				state = One2;
+			} else {
+				state = Init2;
+			} break;
+		default:
+			state = Init2;
+			break;
 	}
 	switch(state) {
 		case(Init2):
-			transmit_data(0x00);
 			break;
-		case(One2): transmit_data(0x01); break;
-		case(Two2): transmit_data(0x02); break;
-		case(Three2): transmit_data(0x04); break;
-		case(Four2): transmit_data(0x08); break;
-		case(Five): transmit_data(0x10); break;
-		case(Six): transmit_data(0x20); break;
-		case(Seven): transmit_data(0x40); break;
-		case(Eight): transmit_data(0x80); break;
+		case(One2):
+			transmit_data(0x01); break;
+		case(Two2):
+			transmit_data(0x02); break;
+		case(Three2):
+			transmit_data(0x04); break;
+		case(Four2):
+			transmit_data(0x08); break;
+		case(Five):
+			transmit_data(0x10); break;
+		case(Six):
+			transmit_data(0x20); break;
+		case(Seven):
+			transmit_data(0x40); break;
+		case(Eight):
+			transmit_data(0x80); break;
 		default: break;
 	}
 	return state;
 }
 
+enum FestiveThird {Init1, One1, Two1, Three1, Four1}; //go == 0x02
+int FestiveThirdSMTick(int state) {
+	switch(state) {
+		case(Init1):
+			if (go == lightstate3) {
+				state = One1;
+			} break;
+		case(One1):
+			if (go == lightstate3) {
+				state = Two1;
+			} else {
+				state = Init1;
+			} break;
+		case(Two1):
+			if (go == lightstate3) {
+				state = Three1;
+			} else {
+				state = Init1;
+			} break;
+		case(Three1):
+			if (go == lightstate3) {
+				state = Four1;
+			} else {
+				state = Init1;
+			} break;
+		case(Four1):
+			if (go == lightstate3) {
+				state = One1;
+			} else {
+				state = Init1;
+			} break;
+		default:
+			state = Init1;
+			break;
+	}
+
+	switch(state) {
+		case(Init1):
+			break;
+		case(One1):
+			transmit_data(0xA0); break;
+		case(Two1):
+			transmit_data(0x0A); break;
+		case(Three1):
+			transmit_data(0x50); break;
+		case(Four1):
+			transmit_data(0x05); break;
+		default: break;
+	}
+	return state;
+}
+
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-		DDRA = 0x00; PORTA = 0xFF;
+	DDRA = 0x00; PORTA = 0xFF;
         DDRC = 0xFF; PORTC = 0x00;
     /* Insert your solution below */
 	static task task1, task2, task3, task4;
 	task *tasks[] = {&task1, &task2, &task3, &task4};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
-	
+
 	const char start = -1;
 	//Task1
 	task1.state = start;
-	task1.period = 100;
+	task1.period = 200;
 	task1.elapsedTime = task1.period;
 	task1.TickFct = &CycleSMTick;
 	//Task2
 	task2.state = start;
-	task2.period = 100;
+	task2.period = 200;
 	task2.elapsedTime = task2.period;
-	task2.TickFct = &FestiveOneSMTick;
+	task2.TickFct = &FestiveFirstSMTick;
 	//Task3
 	task3.state = start;
-	task3.period = 100;
+	task3.period = 200;
 	task3.elapsedTime = task3.period;
-	task3.TickFct = &FestiveTwoSMTick;
+	task3.TickFct = &FestiveThirdSMTick;
 	//Task4
 	task4.state = start;
-	task4.period = 100;
+	task4.period = 200;
 	task4.elapsedTime = task4.period;
-	task4.TickFct = &FestiveThreeSMTick;
-	
-	TimerSet(100);
+	task4.TickFct = &FestiveDefaultSMTick;
+
+	TimerSet(200);
 	TimerOn();
-	
+
 	unsigned short i; //scheduler loop iterator
 	while(1) {
+		tmpA = ~PINA;
 		for (i = 0; i < numTasks; i++) {
 			if (tasks[i] -> elapsedTime == tasks[i] -> period) {
 				tasks[i] -> state = tasks[i] -> TickFct(tasks[i] -> state);
 				tasks[i] -> elapsedTime = 0;
-			} 
-			tasks[i] -> elapsedTime += 100;
+			}
+			tasks[i] -> elapsedTime += 200;
 		}
 		while(!TimerFlag);
 		TimerFlag = 0;
 	}
 	return 0;
-	
+
 }
---
+++ turnin/part2test.c
@@ -0,0 +1,367 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#include "timer.h"
+#include "scheduler.h"
+#endif
+
+void transmit_data(unsigned char data) {
+
+    int i;
+
+    for (i = 0; i < 8 ; ++i) {
+
+   	 // Sets SRCLR to 1 allowing data to be set
+
+   	 // Also clears SRCLK in preparation of sending data
+
+   	 PORTC = 0x08;
+
+   	 // set SER = next bit of data to be sent.
+
+   	 PORTC |= ((data >> i) & 0x01);
+
+   	 // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+
+   	 PORTC |= 0x02;
+
+    }
+
+    // set RCLK = 1. Rising edge copies data from “Shift” register to “Storage” register
+
+    PORTC |= 0x04;
+
+    // clears all lines in preparation of a new transmission
+
+    PORTC = 0x00;
+
+}
+
+//shared variables
+unsigned char go;
+unsigned char button;
+
+enum down_States{down_wait, down_display1, down_display2, down_display3};
+
+int down_Tick(int state){
+	switch(state){
+	case down_wait:
+		if(go == 0x02){
+			state = down_display1;
+		}
+		break;
+	case down_display1:
+		if(go == 0x02){
+		state = down_display2;
+		}
+		else{
+			state = down_wait;
+		}
+		break;
+	case down_display2:
+		if(go == 0x02){
+		state = down_display3;
+		}
+		else {
+			state = down_wait;
+		}
+		break;
+	case down_display3:
+		if(go== 0x02){
+		state = down_display1;
+		}
+		else{
+			state = down_wait;
+		}
+		break;
+	default:
+		state = down_display1;
+		break;
+	}
+	switch(state){
+		case down_wait:
+			break;
+		case down_display1:
+			transmit_data(0xc3);
+			break;
+		case down_display2:
+			transmit_data(0x18);
+			break;
+		case down_display3:
+			transmit_data(0x24);
+			break;
+		default:
+			break;
+	}
+	return state;
+}
+
+enum default_States{default_wait, default_display1, default_display2, default_display3, default_display4};
+
+int default_Tick(int state){
+	switch(state){
+		case default_wait:
+			if(go == 0x00){
+				state = default_display1;
+			}
+			break;
+		case default_display1:
+			if(go == 0x00){
+			state = default_display2;
+			}
+			else{
+				state = default_wait;
+			}
+			break;
+		case default_display2:
+			if(go ==0x00){
+			state = default_display3;
+			}
+			else{
+				state = default_wait;
+			}
+			break;
+		case default_display3:
+			if(go==0x00){
+			state = default_display4;
+			}
+			else{
+				state = default_wait;
+			}
+			break;
+		case default_display4:
+			if(go == 0x00){
+			state = default_display1;
+			}
+			else{
+				state = default_wait;
+			}
+			break;
+		default:
+			state = default_display1;
+			break;
+		}
+	switch(state){
+		case default_wait:
+			break;
+		case default_display1:
+			transmit_data(0xAA);
+			break;
+		case default_display2:
+			transmit_data(0x55);
+			break;
+		case default_display3:
+			transmit_data(0x3C);
+			break;
+		case default_display4:
+			transmit_data(0xA5);
+			break;
+		default:
+			break;
+	}
+	return state;
+}
+
+enum up_States{up_wait, up_display1, up_display2, up_display3};
+
+int up_Tick(int state){
+	switch(state){
+		case up_wait:
+			if(go == 0x01){
+				state = up_display1;
+			}
+			break;
+		case up_display1:
+			if(go == 0x01){
+			state = up_display2;
+			}
+			else{
+				state = up_wait;
+			}
+			break;
+		case up_display2:
+			if(go == 0x01){
+			state = up_display3;
+			}
+			else{
+				state = up_wait;
+			}
+			break;
+		case up_display3:
+			if(go == 0x01){
+			state = up_display1;
+			}
+			else{
+				state = up_wait;
+			}
+			break;
+		default:
+			state = up_display1;
+			break;
+		}
+	switch(state){
+		case up_wait:
+			break;
+		case up_display1:
+			transmit_data(0x18);
+			break;
+		case up_display2:
+			transmit_data(0x66);
+			break;
+		case up_display3:
+			transmit_data(0xE7);
+			break;
+		default:
+			break;
+	}
+	return state;
+}
+
+enum which_SM_States{ down_, default_, up_, off, off_wait};
+
+int which_SM_Tick(int state){
+	switch(state){
+		case default_:
+			if(button == 0x01){
+				state = up_;
+			}
+			else if(button == 0x02){
+				state = down_;
+			}
+			else if(button == 0x03){
+				state = off;
+			}
+			break;
+		case up_:
+			if(button == 0x02){
+				state = default_;
+			}
+			else if(button == 0x03){
+				state = off;
+			}
+			break;
+		case down_:
+			if(button == 0x01){
+				state = default_;
+			}
+			else if(button == 0x03){
+				state = off;
+			}
+			break;
+		case off:
+			if(button == 0x00){
+			state = off_wait;
+			}
+			break;
+		case off_wait:
+			if(button == 0x03){
+				state = default_;
+			}
+			break;
+		default:
+			state = default_;
+			break;
+		}
+	switch(state){
+		case default_:
+			go = 0x00;
+			break;
+		case up_:
+			go = 0x01;
+			break;
+		case down_:
+			go = 0x02;
+			break;
+		case off:
+			go = 0x03;
+			transmit_data(0x00);
+			break;
+		case off_wait:
+			break;
+		}
+	return state;
+}
+
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xff;
+	DDRB = 0xff; PORTB = 0x00;
+	DDRC = 0xff; PORTC = 0x00;
+    /* Insert your solution below */
+	button = 0x00;
+
+	static task task1, task2, task3, task4;
+	task *tasks[] = {&task1, &task2, &task3, &task4};
+const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+	const char start = -1;
+
+	task1.state = start;
+	task1.period = 200;
+	task1.elapsedTime = task1.period;
+	task1.TickFct =&down_Tick;
+
+	task2.state = start;
+	task2.period = 200;
+	task2.elapsedTime = task2.period;
+	task2.TickFct = &default_Tick;
+
+	task3.state= start;
+	task3.period= 200;
+	task3.elapsedTime = task3.period;
+	task3.TickFct = &up_Tick;
+
+	task4.state = start;
+	task4.period = 200;
+	task4.elapsedTime = task4.period;
+	task4.TickFct= which_SM_Tick;
+
+	unsigned long int findGCD(unsigned long int a, unsigned long int b){
+		unsigned long int c;
+		while(1){
+			c = a%b;
+			if (c==0){
+				return b;
+			}
+			a = b;
+			b = c;
+		}
+		return 0;
+	}
+	unsigned short i;
+	unsigned long GCD = tasks[0]->period;
+	for(i=1; i< numTasks; i++){
+		GCD = findGCD(GCD, tasks[i]->period);
+	}
+
+	TimerSet(GCD);
+	TimerOn();
+
+
+    /* Insert your solution below */
+    while (1) {
+	button = ~PINA;
+	for(i=0; i<numTasks; i++){
+		if(tasks[i]->elapsedTime == tasks[i]->period){
+			tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+			tasks[i]->elapsedTime = 0;
+		}
+		tasks[i]->elapsedTime += GCD;
+	}
+	while(!TimerFlag){};
+	TimerFlag = 0;
+    }
+
+    return 1;
+}
